881655541a00d742f81b1128fe5efeb2bdb45509
m1n0 <mino.lukac@gmail.com>
2016-06-16 20:43:21 +0200

    04 Delete unused code

--- 01.learnyournode/04.io-async.js
+++ 01.learnyournode/04.io-async.js
@@ -4,5 +4,3 @@ fs.readFile(process.argv[2], 'utf8', function(err, data) {
   if (err) throw err;
   console.log(data.split('\n').length - 1);
 })
-
-// console.log(fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1);
