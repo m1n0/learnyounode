3bbdc7f86acbd07430e69353c5298c4a89694bcb
m1n0 <mino.lukac@gmail.com>
2016-06-16 21:36:33 +0200

    07 Simplest solution that passes the test, but I dont understand why 'end' event doesnt need to be catched here

--- 01.learnyournode/07.http.js
+++ 01.learnyournode/07.http.js
@@ -3,18 +3,6 @@ var http = require('http')
 http.get(process.argv[2], function(response) {
   response.setEncoding('utf8')
 
-  response.on('data', function(data) {
-    if (data)
-      console.log(data)
-  })
-
-  response.on('error', function(data) {
-    if (data)
-      console.log(data)
-  })
-
-  response.on('end', function(data) {
-    if (data)
-      console.log(data)
-  })
+  response.on('data', console.log)
+  response.on('error', console.error)
 })
